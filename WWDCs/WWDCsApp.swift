df8f4f41e44c52f571ac405c742078c96b55dd50
Nicolò Curioni <nicolo.curioni96@gmail.com>
2024-04-19 17:52:41 +0200

    Initial Commit

---
+++ WWDCs/WWDCsApp.swift
@@ -0,0 +1,32 @@
+//
+//  WWDCsApp.swift
+//  WWDCs
+//
+//  Created by Nicolò Curioni on 19/04/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct WWDCsApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
